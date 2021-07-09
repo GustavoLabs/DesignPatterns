2bc70b97da48e6aadff721679b061c6a59d2a4e0
unknown <gusta.rgouvea@gmail.com>
2021-07-09 17:42:25 -0300

    Design Pattern - Abstract Factory Example

---
+++ src/abstractFactory/Main.java
@@ -0,0 +1,27 @@
+package abstractFactory;
+
+import abstractFactory.cars.Car;
+import abstractFactory.factory.AbstractFactory;
+import abstractFactory.factory.Type;
+import abstractFactory.motorcycle.Motorcycle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AbstractFactory factory = AbstractFactory.getFactory(Type.BMW);
+        Car BMWCar = factory.createCar();
+        Motorcycle BMWMoto = factory.createMotorcycle();
+        System.out.println(BMWMoto);
+        System.out.println(BMWCar);
+
+        factory = AbstractFactory.getFactory(Type.HONDA);
+
+        Car HondaCar = factory.createCar();
+        Motorcycle HondaMoto = factory.createMotorcycle();
+
+        System.out.println(HondaCar);
+        System.out.println(HondaMoto);
+
+    }
+}
